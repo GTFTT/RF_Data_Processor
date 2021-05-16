5442c79dfb0fa84b9633b8975456183b085f09dc
GT <FTowerT@outlook.com>
2021-05-17 01:53:48 +0300

    initialized

---
+++ RF_Data_Processor.cpp
@@ -0,0 +1,166 @@
+#include "RF_Data_Processor.h"
+
+RF_Data_Processor::RF_Data_Processor() {
+  TRANSMITTING_DELAY = 60;
+  RECEIVING_DELAY = 10;
+  SETUP_DELAY = 60;
+}
+
+//----- Public ----------------------------------------------------------------------------------------------
+
+void RF_Data_Processor::initializeRadio(char readingAddress[], char writingAddress[]) {
+  setReadingPipeAddress(readingAddress);
+  setWritingPipeAddress(writingAddress);
+
+  _radio->begin();
+}
+
+
+void RF_Data_Processor::setWritingPipeAddress(char address[]) {
+  for(int i = 0; i < sizeof(WritingPipeAddress); i++) {
+    WritingPipeAddress[i] = (byte) address[i];
+  }
+}
+
+
+void RF_Data_Processor::setReadingPipeAddress(char address[]) {
+  for(int i = 0; i < sizeof(ReadingPipeAddress); i++) {
+    ReadingPipeAddress[i] = (byte) address[i];
+  }
+}
+
+void RF_Data_Processor::setupRadioForWriting() {
+  if(_radio != NULL) {
+    _radio -> stopListening();
+    _radio -> openWritingPipe(WritingPipeAddress);
+    delay(SETUP_DELAY);
+  }
+}
+
+void RF_Data_Processor::setupRadioForReading() {
+  if(_radio != NULL) {
+    _radio -> startListening();
+    _radio -> openReadingPipe(1, ReadingPipeAddress );
+    delay(SETUP_DELAY);
+  }
+}
+
+/*Receive message with RF communication*/
+String RF_Data_Processor::receive(void) {
+  String result = "";
+    while (_radio -> available()) {
+        char text[32] = "";
+        _radio -> read(&text, sizeof(text));
+        result = result + text;
+    }
+    return result;
+}
+
+void RF_Data_Processor::sendJson(char* message, int messageSize) {
+  //CONSTANT VALUES FOR PACKS---------------------------------
+  int code = 1;
+  int packNo = 1;
+  const size_t ARRAY_CAPACITY_FOR_PACK = JSON_ARRAY_SIZE(3);// compute the required size
+  //----------------------------------------------------------
+
+  //PREPARE---------------------------------------------------
+  bool isInitPack = true;
+  bool sending = true;
+  int lastIndex = 0;
+  char payload[messageSize];
+  for(int g = 0; g < messageSize; g++)payload[g] = (message[g] != '\"')? message[g]: '$'; //Copy message to another array and replace all forbidden characters
+  String payloadString = String(payload);
+  //----------------------------------------------------------
+
+  while(sending) {
+    // 1. Generate empty json
+    DynamicJsonDocument emptyDocPack(64);
+    //generate meta
+    StaticJsonDocument< ARRAY_CAPACITY_FOR_PACK > initMetaArrayDoc;// allocate the memory for the document
+    JsonArray initMetaArray = initMetaArrayDoc.to<JsonArray>();// create an empty array
+    initMetaArray.add(0); //Any number between 0 and 9, needed just to calculate size
+    initMetaArray.add(packNo);
+    initMetaArray.add(lastPackId);
+    emptyDocPack["m"] = initMetaArrayDoc;
+    emptyDocPack["d"] = "";
+
+    // 2. Measure its actual size in bytes and calculate required data
+    int emptyPackSize = measureJson(emptyDocPack);
+    int availablePayloadSize = 32 - emptyPackSize - 2;//Minus two because we will transform it into an array later
+
+    // 3. Generate data snippet(from payload)
+    String payloadSnippet = payloadString.substring(lastIndex, lastIndex + availablePayloadSize);
+    lastIndex = lastIndex + availablePayloadSize;
+
+    // 4. Create actually object to save data input
+    DynamicJsonDocument docPack(64);
+    StaticJsonDocument< ARRAY_CAPACITY_FOR_PACK > metaArrayDoc;//allocate the memory for the document
+    JsonArray metaArray = metaArrayDoc.to<JsonArray>();// create an empty array
+    //Set code( 1-one message is a full pack, 2-beginning of the message, 3-somewhere in the middle, 4-end of the message(last pack))
+    if(availablePayloadSize >= payloadString.length()) {
+      metaArray.add(1);
+    } else if(isInitPack) {
+      metaArray.add(2);
+      isInitPack = false;
+    }  else if(lastIndex >= payloadString.length()) {
+      metaArray.add(4);
+    } else {
+      metaArray.add(3);
+    }
+    metaArray.add(packNo);
+    metaArray.add(lastPackId);
+    docPack["m"] = metaArrayDoc;
+    docPack["d"] = payloadSnippet; //Insert payload snippet
+    
+    // 5. Serialize pack
+    String finishedPack;
+    serializeJson(docPack, finishedPack);
+    char finishedPackArr[finishedPack.length()+1];
+    finishedPack.toCharArray(finishedPackArr, sizeof(finishedPackArr));
+
+    // 6. Send finished pack
+    send(finishedPackArr, sizeof(finishedPackArr));
+    
+    // 7. If all payload was sent - stop sending
+    if(lastIndex >= payloadString.length()) sending = false;
+    packNo++;
+  }
+  
+  //Maximum pack id is 99
+  getLastPackId();
+}
+
+void RF_Data_Processor::send(String message) {
+  //If message is longer than 32 bytes we have to send many packs
+  for(int i = 0; i < ceil(message.length()/32.0); i++) {
+    String snippetToSend = message.substring(i*32, (i+1)*32);
+
+    char chArr[32];
+    snippetToSend.toCharArray(chArr, sizeof(chArr));
+    _radio -> write(&chArr, sizeof(chArr));
+    delay(TRANSMITTING_DELAY); //Time to perform transmitting
+  }
+}
+
+void RF_Data_Processor::send(char* message, int messageSize) {
+  int countOfPacks = ceil(messageSize/32.0);
+
+  //If message is longer than 32 bytes we have to send many packs
+  for(int i = 0; i < countOfPacks; i++) {
+    int numOfBytesToSend = ((i+1) == countOfPacks)? messageSize%32: 32;
+    char* pointer = message + (i*32);
+    _radio -> write(pointer, numOfBytesToSend);
+
+    delay(TRANSMITTING_DELAY); //Time to perform transmitting
+  }
+}
+
+
+//----- Private ---------------------------------------------------------------------------------------------
+
+int RF_Data_Processor::getLastPackId(void) {
+  int res = lastPackId;
+  lastPackId++;
+  if(lastPackId > 99) lastPackId = 1;
+  return res;
+}
---
+++ RF_Data_Processor.h
@@ -0,0 +1,82 @@
+#ifndef RF_Data_Processor_h
+#define RF_Data_Processor_h
+
+//Standard stuff
+#include "Arduino.h"
+
+#include <SPI.h>                  /* to handle the communication interface with the modem*/
+#include <nRF24L01.h>             /* to handle this particular modem driver*/
+#include <RF24.h>                 /* the library which helps us to control the radio modem*/
+#include <ArduinoJson.h>          /* Process JSON documents, serialization, packs and other*/
+
+#define SCK  13
+#define MISO 12
+#define MOSI 11
+#define CSN  8
+#define CE   7
+
+/*
+  RF_Data_Processor - Use to convert data into special strings structure and decode it later into values.
+  There are also available methods to send raw data.
+
+  The protocol used by this library is not the best yet, it transmits data by packages of max size 32 bytes.
+  It is not so efficient but it have to work anyway. Future protocol have to be changed to be faster,
+  less weight and more compact;
+
+  Current protocol: {"m": [1, 2, 3], "d": ""}
+    - "m" stands for meta, contains data in that order(type of payload, package number, package id)
+    - "d" stands for data, there is actually piece of JSON document we wont to transmit
+  
+  Future protocol: | serialized meatdata here... | serialized data here... |
+
+  Created by:    GT
+  Creation date: 03.05.2021
+  Last edited:   17.05.2021
+*/
+class RF_Data_Processor
+{
+  public:
+    /*Initialize default values*/
+    RF_Data_Processor(void);
+
+    /*Must be called to initialize all required radio variables*/
+    void initializeRadio(char readingAddress[]="00009", char writingAddress[]="00008");//Default reading and writing pipe addresses
+
+    /*Makes all needed operations to prepare radio for writing*/
+    void setupRadioForWriting(void);
+
+    /*Makes all needed operations to prepare radio for reading*/
+    void setupRadioForReading(void);
+
+    /*Set writing pipe adderes, provide array with 5 characters and one closing character(char[5] = "00008")*/
+    void setWritingPipeAddress(char address[]);
+
+    /*Set reading pipe adderes, provide array with 5 characters and one closing character(char[5] = "00009")*/
+    void setReadingPipeAddress(char address[]);
+
+    /*Call dhis to receive message instantly and return it back to the user*/
+    String receive(void);
+
+    /*Send json in special format, it will be decoded later after receiving*/
+    void sendJson(char message[], int messageSize);
+
+    /*Send raw array of characters*/
+    void send(char message[], int messageSize);
+    
+    /*Send raw string*/
+    void send(String message);
+  private:
+    static RF24* _radio; //Static pointer is used because it does not disapper
+
+    int TRANSMITTING_DELAY; //Time for transmitting a message
+    int RECEIVING_DELAY; //Time for receiving a message
+    int SETUP_DELAY; //Time for setup modem
+    int lastPackId = 1; //Last id of sent pack(99 is maximum value and 1 is minimum)
+    byte WritingPipeAddress[6] = {}; /* Address to which data to be transmitted*/
+    byte ReadingPipeAddress[6] = {}; /* Address from which we receive messages */
+
+    /*Each time this function is called it increments lastPackId, but the maximum pack id is 99 and min value is 1*/
+    int RF_Data_Processor::getLastPackId(void);
+};
+
+#endif
